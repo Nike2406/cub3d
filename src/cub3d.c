b93af3018144506ce61bd76ad241d7d47f245b28
prochell <prochell@student.21-school.ru>
2021-12-05 18:26:48 +0300

    remove less

--- src/cub3d.c
+++ src/cub3d.c
@@ -6,48 +6,11 @@
 /*   By: prochell <prochell@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/11/08 20:22:16 by prochell          #+#    #+#             */
-/*   Updated: 2021/12/04 23:10:06 by prochell         ###   ########.fr       */
+/*   Updated: 2021/12/05 18:24:07 by prochell         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../cub3d.h"
-
-void	get_hook(t_all *data)
-{
-	mlx_hook(data->win->win, 2, 1L<<0, deal_key, data);
-	mlx_hook(data->win->win, 17, 0, keys_err, data);
-}
-
-void	first_render(t_win *win, t_all *data)
-{
-	win->mlx = mlx_init();
-	win->win = mlx_new_window(win->mlx, win->img_width, \
-		win->img_height, "cub3d");
-	win->img = mlx_new_image(win->mlx, win->img_width, \
-		win->img_height);
-	win->addr = mlx_get_data_addr(win->img, &win->bits_per_pixel,\
-		&win->line_length, &win->endian);
-	// draw(data);
-	mlx_put_image_to_window(win->mlx, win->win, win->img, 0, 0);
-	get_hook(data);
-	mlx_loop(win->mlx);
-}
-
-void	render(t_win *win, t_all *data)
-{
-	void	*tmp;
-
-	tmp = win->img;
-	win->img = mlx_new_image(win->mlx, win->img_width, \
-		win->img_height);
-	win->addr = mlx_get_data_addr(win->img, &win->bits_per_pixel,\
-		&win->line_length, &win->endian);
-	mlx_clear_window(win->mlx, win->win);
-	// draw(data);
-	mlx_put_image_to_window(win->mlx, win->win, win->img, 0, 0);
-	get_hook(data);
-	mlx_destroy_image(win->mlx, tmp);
-}
 
 int	main(int argc, char **argv)
 {
--- src/old/draw1.c
+++ src/old/draw1.c
@@ -12,6 +12,44 @@
 
 // #include "../cub3d.h"
 
+
+// void	get_hook(t_all *data)
+// {
+// 	mlx_hook(data->win->win, 2, 1L<<0, deal_key, data);
+// 	mlx_hook(data->win->win, 17, 0, keys_err, data);
+// }
+
+// void	first_render(t_win *win, t_all *data)
+// {
+// 	win->mlx = mlx_init();
+// 	win->win = mlx_new_window(win->mlx, win->img_width, \
+// 		win->img_height, "cub3d");
+// 	win->img = mlx_new_image(win->mlx, win->img_width, \
+// 		win->img_height);
+// 	win->addr = mlx_get_data_addr(win->img, &win->bits_per_pixel,\
+// 		&win->line_length, &win->endian);
+// 	// draw(data);
+// 	mlx_put_image_to_window(win->mlx, win->win, win->img, 0, 0);
+// 	get_hook(data);
+// 	mlx_loop(win->mlx);
+// }
+
+// void	render(t_win *win, t_all *data)
+// {
+// 	void	*tmp;
+
+// 	tmp = win->img;
+// 	win->img = mlx_new_image(win->mlx, win->img_width, \
+// 		win->img_height);
+// 	win->addr = mlx_get_data_addr(win->img, &win->bits_per_pixel,\
+// 		&win->line_length, &win->endian);
+// 	mlx_clear_window(win->mlx, win->win);
+// 	// draw(data);
+// 	mlx_put_image_to_window(win->mlx, win->win, win->img, 0, 0);
+// 	get_hook(data);
+// 	mlx_destroy_image(win->mlx, tmp);
+// }
+
 // void	print_player(t_win *win, int start_x, int start_y, int color)
 // {
 // 	int	y;
